5f57bffdd3e976d3f6eae20b3a6c9ddd2ed5c445
Pamella Fernandes <88452990+pamellamoff@users.noreply.github.com>
2024-05-10 11:27:53 -0300

    Update ajuste_ferias.sql

--- ajuste_ferias.sql
+++ ajuste_ferias.sql
@@ -1,8 +1,8 @@
 /*
-  Consulta exclui todas as linhas da tabela "sdx_excelencia_comercial.camp_incentivo__rede_vigente.int__participantes_dia_util" com a data, na qual, o colaborador se 
-  encontra de férias. Essa tabela armazena os dias uteis do mês em que cada colaborador estava ativo no ponto de atendimento. Ela, por sua vez, é usada como base para o
-  cálculo de metas indivíduais.
-  By: Pâmella
+Consulta exclui todas as linhas da tabela "sdx_excelencia_comercial.camp_incentivo__rede_vigente.int__participantes_dia_util" com a data, na qual, o colaborador se 
+encontra de férias. Essa tabela armazena os dias uteis do mês em que cada colaborador estava ativo no ponto de atendimento. 
+Ela, por sua vez, é usada como base para o cálculo de metas indivíduais.
+By: Pâmella
 */
 
 SET periodo = '%CAMPANHA MAI/24%';
@@ -25,7 +25,8 @@ maximo_ferias AS
     GROUP BY ALL
 )
 
-/*Retorna os demais campos do registro de solicitação de férias mais atual do colaborador e converte os tipos de data para o formato DATE yyyy-mm-dd*/
+/*Retorna os demais campos do registro de solicitação de férias mais atual do colaborador e converte os tipos de data para
+o formato DATE yyyy-mm-dd.*/
 ,ferias AS
 (
     SELECT 
@@ -44,11 +45,49 @@ maximo_ferias AS
         AND tabela.dta_solicitacao = maximo_ferias.dta_solicitacao
 )
 
-/*Retorna os reigstros da tabela "sdx_excelencia_comercial.camp_incentivo__rede_vigente.int__participantes_dia_util" nos quais o colaborador se encontra de férias*/
+/*Retorna os dados dos colaboradores que podem constar na tabela 
+"sdx_excelencia_comercial.camp_incentivo__rede_vigente.int__participantes_dia_util", ou seja, apenas gerente de contas e 
+produtos e agentes comerciais. Isso porque, apenas esses cargos possuem metas individuais.*/
+,empregado AS
+(
+    SELECT
+        empregado.idt_emp
+        ,empregado.num_mat_epg
+        ,empregado.idt_cgo
+        ,empregado.dta_ads
+        ,empregado.dta_dms
+        ,empregado.num_cen_cst
+        ,cargo.nom_cgo
+        ,cargo_basico.nom_cgo_bse
+        
+    FROM sdx_excelencia_comercial.sdx_dominios.stg_administrativo_recursos_humanos__empregado AS empregado
+
+    LEFT JOIN sdx_excelencia_comercial.sdx_dominios.stg_administrativo_recursos_humanos__cargo  AS cargo
+      ON cargo.idt_emp = empregado.idt_emp
+      AND cargo.idt_cgo = empregado.idt_cgo
+
+    LEFT JOIN sdx_excelencia_comercial.sdx_dominios.stg_administrativo_recursos_humanos__cargo_basico AS cargo_basico 
+      ON cargo_basico.idt_cgo_bse = cargo.idt_cgo_bse
+
+    WHERE 
+    (
+      (empregado.num_cen_cst IS NOT NULL)
+      AND (empregado.dta_dms IS NULL)
+      AND (empregado.idt_emp IN ('2','27'))
+      AND (cargo_basico.idt_cgo_bse IN (2764,2067,2344,3133,3134))
+    ) -- gerente de agencia nao entra na tabela de dias uteis, gerente de contas e produtos e agentes comerciais apenas que entram na tabela de dias uteis.
+)
+
+/*Retorna os reigstros da tabela "sdx_excelencia_comercial.camp_incentivo__rede_vigente.int__participantes_dia_util" nos 
+quais o colaborador se encontra de férias, retirando da consulta as solicitações de colaboradores que não se enquadram nos
+cargos de gerente de contas e produtos e agentes comerciais.*/
 ,dias_uteis AS
 (
     SELECT 
         ferias.campanha
+        ,CONCAT(int_participantes_dia_util.matricula,'-',
+                int_participantes_dia_util.num_dnd,'-',
+                int_participantes_dia_util.data_movimento) AS chave
         ,ferias.matricula AS matricula_ferias
         ,int_participantes_dia_util.matricula AS matricula_dia_util
         ,int_participantes_dia_util.num_dnd
@@ -57,28 +96,41 @@ maximo_ferias AS
         ,int_participantes_dia_util.data_movimento
     
     FROM ferias
-    
+
+    INNER JOIN empregado
+        ON empregado.num_mat_epg = CAST(RIGHT(ferias.matricula,5) AS INT)
+  
     LEFT JOIN sdx_excelencia_comercial.camp_incentivo__rede_vigente.int__participantes_dia_util AS int_participantes_dia_util
         ON int_participantes_dia_util.matricula = ferias.matricula
         AND int_participantes_dia_util.data_movimento >= ferias.dta_saida
         AND int_participantes_dia_util.data_movimento < ferias.dta_retorno
     
-    WHERE int_participantes_dia_util.matricula IS NOT NULL 
+    WHERE int_participantes_dia_util.matricula IS NOT NULL -- garante que apenas os casos de férias venham na consulta, inserções não contam aqui.
     
     ORDER BY
        int_participantes_dia_util.matricula
        ,int_participantes_dia_util.data_movimento
 )
 
-/*
-  Exclusão dos registros da tabela "sdx_excelencia_comercial.camp_incentivo__rede_vigente.int__participantes_dia_util" que compreende o período de férias do colaborador, ou
-  seja, os dias que ele não se encontra ativo no ponto de atendimento.
-*/
-SELECT * FROM dias_uteis
+SELECT * FROM dias_uteis -- apenas para visualização
 
+/*Exclusão dos registros da tabela "sdx_excelencia_comercial.camp_incentivo__rede_vigente.int__participantes_dia_util" 
+que compreende o período de férias do colaborador, ou seja, os dias que ele não se encontra ativo no ponto de atendimento.*/
 DELETE 
 FROM sdx_excelencia_comercial.camp_incentivo__rede_vigente.int__participantes_dia_util AS int_participantes_dia_util
-WHERE 
-    int_participantes_dia_util.matricula = dias_uteis.matricula_dia_util
-    AND int_participantes_dia_util.num_dnd = dias_uteis.num_dnd
-    AND int_participantes_dia_util.data_movimento = dias_uteis.data_movimento
+WHERE CONCAT(int_participantes_dia_util.matricula,'-',
+            int_participantes_dia_util.num_dnd,'-',
+            int_participantes_dia_util.data_movimento)
+IN
+(
+    SELECT dias_uteis.chave FROM dias_uteis
+
+    INNER JOIN sdx_excelencia_comercial.camp_incentivo__rede_vigente.int__participantes_dia_util AS int_participantes_dia_util
+        ON CONCAT(int_participantes_dia_util.matricula,'-',
+                 int_participantes_dia_util.num_dnd,'-',
+                 int_participantes_dia_util.data_movimento) = dias_uteis.chave
+)
+    
+ORDER BY
+   int_participantes_dia_util.matricula
+   ,int_participantes_dia_util.data_movimento  
